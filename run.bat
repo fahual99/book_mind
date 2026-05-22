067909c12c59b7e7430df97bbfbc7964c4bb10e1
Faisal Alghamdi <fahaalghamdi@gmail.com>
2026-05-22 06:45:19 +0300

    removed notebook

--- run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-start cmd /k "cd /d C:\Users\fahaa\OneDrive\Desktop\book_webapp\frontend && npm run dev"
-
-start cmd /k "cd /d C:\Users\fahaa\OneDrive\Desktop\book_webapp\backend && python -m uvicorn app.main:app --reload --port 8000"
-
-start cmd /k "ngrok && ngrok http 3000"
